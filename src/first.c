11ee099cdd7cad2ae73bfb5d4963d66a4ebd021b
MartinDimanche <102289291+MartinDimanche@users.noreply.github.com>
2022-03-25 13:17:53 +0100

    Add files via upload

---
+++ src/first.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //Permet l'entré et sortie (écrire un message à l'écran/afficher un message)
+#include <stdlib.h> //Ajoute des bibliothèques supplémentaires permettant notamment l'allocation dynamique
+
+int main()
+{
+    printf("Hello\n");
+    return 0;
+}
