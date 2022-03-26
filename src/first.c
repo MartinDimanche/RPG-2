b8b9b12229845db3bd5286607718a980bc1d3155
Skalefou <skamsagar@gmail.com>
2022-03-26 21:07:43 +0100

    test 6

--- src/first.c
+++ src/first.c
@@ -1,6 +1,6 @@
 #include <stdio.h> //Permet l'entré et sortie (écrire un message à l'écran/afficher un message)
 #include <stdlib.h> //Ajoute des bibliothèques supplémentaires permettant notamment l'allocation dynamique
-//Test2
+//Test5
 void testDisplay();
 
 int main()
