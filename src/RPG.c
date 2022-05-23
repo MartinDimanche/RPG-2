34abb3e3fff8df97b98223c548e46b5826df6314
Skalefou <skamsagar@gmail.com>
2022-05-23 16:17:16 +0200

    Integration Menu

--- src/RPG.c
+++ src/RPG.c
@@ -1,10 +1,9 @@
 #include "RPG.h"
+#include "menu.h"
 
-struct RPG rpg;
+static struct RPG rpg;
 
 void RPG_Run()
 {
-    rpg.a = 4;
+    Menu_run();
 }
-
-
---
+++ src/menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+struct Menu
+{
+    
+};
+
+void Menu_run();
+
+#endif
